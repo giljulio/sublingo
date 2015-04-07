ae7087199a2f77a676cc512e4fff664f9afea4b3
Gil Julio <gilsjulio@gmail.com>
2015-04-07 12:27:35 +0100

    First commit, server split mostly done

---
+++ server/src/com/giljulio/sublingo/Server.java
@@ -0,0 +1,61 @@
+package com.giljulio.sublingo;
+
+import com.giljulio.sublingo.task.impl.SplitterTask;
+
+import java.nio.file.*;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Created by Gil on 03/01/15.
+ */
+public class Server {
+
+    /**
+     * Logger instance.
+     */
+    private static final Logger logger = Logger.getLogger(Server.class.getName());
+
+    private static Engine engine;
+
+    public static void main(String[] args){
+        engine = new Engine();
+        engine.start();
+        registerDirectoryListerner();
+    }
+
+
+    private static void registerDirectoryListerner(){
+        //define a folder root
+        Path path = Paths.get("data/processing");
+
+        try {
+            WatchService ws = path.getFileSystem().newWatchService();
+            path.register(ws, StandardWatchEventKinds.ENTRY_CREATE,
+                    StandardWatchEventKinds.ENTRY_DELETE,
+                    StandardWatchEventKinds.ENTRY_MODIFY);
+            WatchKey watch = null;
+            while (true) {
+                try {
+                    watch = ws.take();
+                } catch (InterruptedException ex) {
+                    logger.severe("Interrupted");
+                }
+                List<WatchEvent<?>> events = watch.pollEvents();
+                watch.reset();
+                for (WatchEvent<?> event : events) {
+                    WatchEvent.Kind<Path> kind = (WatchEvent.Kind<Path>) event.kind();
+                    Path context = (Path) event.context();
+                    if (kind.equals(StandardWatchEventKinds.ENTRY_CREATE)) {
+                        logger.info("Detected: " + context.getFileName());
+                        engine.pushTask(new SplitterTask(context.getFileName()));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.severe("Server error: " + e.toString());
+        }
+
+    }
+}
+
